d5dc294214f55cc29fb783b9468220608c5310c8
tzuvela <tino.zuvela@gmail.com>
2025-05-02 19:55:32 +0200

    refactored memory_test.go

--- system/memory_test.go
+++ system/memory_test.go
@@ -1,16 +1,30 @@
 package system
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGetMemoryInfo(t *testing.T) {
-	stats, err := GetMemoryInfo()
-	if err != nil {
-		t.Fatalf("GetMemoryInfo returned error: %v", err)
+	tests := []struct {
+		name string
+	}{
+		{name: "valid memory info"},
+		//TODO add more mock test cases
 	}
-	if stats.Total == 0 {
-		t.Error("expected Total > 0")
-	}
-	if stats.UsedPercent < 0 || stats.UsedPercent > 100 {
-		t.Errorf("UsedPercent out of range: %v", stats.UsedPercent)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			stats, err := GetMemoryInfo()
+			if err != nil {
+				t.Fatalf("GetMemoryInfo returned error: %v", err)
+			}
+			if stats.Total == 0 {
+				t.Error("expected Total > 0")
+			}
+			if stats.UsedPercent < 0 || stats.UsedPercent > 100 {
+				t.Errorf("UsedPercent out of range: %v", stats.UsedPercent)
+			}
+		})
+
 	}
 }
